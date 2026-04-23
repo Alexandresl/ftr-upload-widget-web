ce857093cdb8530dd7f37dbd8295b267b586afb1
Alexandre Lima <al3xandr3sl@gmail.com>
2026-04-23 19:50:03 -0300

    Utilizando immer com Zustand

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { enableMapSet } from 'immer'
+import { immer } from "zustand/middleware/immer";
 
 export type Upload = {
     name: string
@@ -10,27 +12,29 @@ type UploadState = {
     addUploads: (files: File[]) => void
 }
 
-export const useUploads = create<UploadState>((set, get) => {
+enableMapSet()
 
-    function addUploads(files: File[]) {
-        for (const file of files) {
-            const uploadId = crypto.randomUUID()
+export const useUploads = create<UploadState, [['zustand/immer', never]]>(
+    immer((set) => {
+        function addUploads(files: File[]) {
+            for (const file of files) {
+                const uploadId = crypto.randomUUID()
 
-            const upload: Upload = {
-                name: file.name,
-                file
+                const upload: Upload = {
+                    name: file.name,
+                    file
+                }
+
+                set(state => {
+                    state.uploads.set(uploadId, upload)
+                })
+
             }
-
-            set(state => {
-                return { uploads: state.uploads.set(uploadId, upload) }
-            })
-
         }
-    }
 
-    return {
-        uploads: new Map(),
-        addUploads
-    }
+        return {
+            uploads: new Map(),
+            addUploads
+        }
 
-})
+    }))
